fdc521e040a4e196721db32c4aafee905342fd06
ChakornK <chakornk2007@gmail.com>
2026-02-19 18:40:46 -0800

    fix: more stable ad skipping

--- app/src/main/assets/unspot/adblock.js
+++ app/src/main/assets/unspot/adblock.js
@@ -1,5 +1,4 @@
 const originalCreateElement = document.createElement;
-let oldVolume = null;
 document.createElement = (() => {
   return function () {
     const element = originalCreateElement.apply(this, arguments);
@@ -7,22 +6,21 @@ document.createElement = (() => {
       const oldPlay = element.play;
       element.play = function () {
         if (!this.src.startsWith("blob:https://open.spotify.com/") && this.duration < 40) {
-          window.adblockDebug = this;
           this.dispatchEvent(new Event("play"));
+          const oldSrc = this.src.toString();
           setTimeout(() => {
             this.currentTime = this.duration;
             this.dispatchEvent(new Event("timeupdate"));
-            const oldSrc = this.src;
             const inter = setInterval(() => {
               if (this.src !== oldSrc) return clearInterval(inter);
               this.currentTime = this.duration;
               this.dispatchEvent(new Event("ended"));
-            }, 20);
+            }, 100);
             setTimeout(() => {
               try {
                 clearInterval(inter);
               } catch {}
-            }, 10000);
+            }, 2000);
           }, 20);
         } else {
           oldPlay.apply(this, arguments);
