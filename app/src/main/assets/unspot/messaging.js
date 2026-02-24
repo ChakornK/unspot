f6dfe5a821fecceda9ae9888d9be30b01dc358fc
ChakornK <chakornk2007@gmail.com>
2026-02-23 16:35:50 -0800

    fix: place hook in messaging.js

--- app/src/main/assets/unspot/messaging.js
+++ app/src/main/assets/unspot/messaging.js
@@ -1,51 +1,57 @@
-let _Platform;
+let Platform;
+(() => {
+  window.webpackChunkclient_web = window.webpackChunkclient_web || [];
 
-window.webpackChunkclient_web = window.webpackChunkclient_web || [];
+  const definePlatform = () => {
+    Object.defineProperty(window, "Platform", { value: Object.getOwnPropertyNames(Platform).reduce((p, c) => ({ ...p, [c]: Platform[c] }), {}) });
+  };
 
-window.webpackChunkclient_web.push([
-  ["__platform_hook__"],
-  {},
-  function installHook(require) {
-    const originalD = require.d.bind(require);
-    require.d = function (exports, descriptors) {
-      if (Object.prototype.hasOwnProperty.call(descriptors, "createPlatformWeb")) {
-        const originalGetter = descriptors.createPlatformWeb;
-        descriptors = Object.assign({}, descriptors, {
-          createPlatformWeb: function () {
-            const originalFn = originalGetter();
-            return async function createPlatformWeb(...args) {
-              const platform = await originalFn.apply(this, args);
-              _Platform = platform;
-              return platform;
-            };
-          },
-        });
-      }
-      originalD(exports, descriptors);
-    };
-  },
-]);
-
-const Platform = new Proxy(
-  {},
-  {
-    get: function (_, prop) {
-      if (!_Platform) return undefined;
-      if (prop === "then") return Promise.resolve(_Platform);
-      return _Platform.getRegistry()._map.get(Symbol.for(prop)).instance;
+  window.webpackChunkclient_web.push([
+    ["__platform_hook__"],
+    {},
+    function installHook(require) {
+      const originalD = require.d.bind(require);
+      require.d = function (exports, descriptors) {
+        if (Object.prototype.hasOwnProperty.call(descriptors, "createPlatformWeb")) {
+          const originalGetter = descriptors.createPlatformWeb;
+          descriptors = Object.assign({}, descriptors, {
+            createPlatformWeb: function () {
+              const originalFn = originalGetter();
+              return async function createPlatformWeb(...args) {
+                const platform = await originalFn.apply(this, args);
+                window._Platform = platform;
+                definePlatform();
+                return platform;
+              };
+            },
+          });
+        }
+        originalD(exports, descriptors);
+      };
     },
-    ownKeys: function () {
-      if (!_Platform) return [];
-      return [
-        ..._Platform
-          .getRegistry()
-          ._map.keys()
-          .map((k) => Symbol.keyFor(k)),
-      ];
+  ]);
+
+  Platform = new Proxy(
+    {},
+    {
+      get: function (_, prop) {
+        if (!window._Platform) return undefined;
+        if (prop === "then") return Promise.resolve(window._Platform);
+        return window._Platform.getRegistry()._map.get(Symbol.for(prop)).instance;
+      },
+      ownKeys: function () {
+        if (!window._Platform) return [];
+        return [
+          ...window._Platform
+            .getRegistry()
+            ._map.keys()
+            .map((k) => Symbol.keyFor(k)),
+        ];
+      },
     },
-  },
-);
-window.Platform = Platform;
+  );
+  window.Platform = Platform;
+})();
 
 let npb = document.querySelector("aside");
 
